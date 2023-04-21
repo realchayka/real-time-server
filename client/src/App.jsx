c2ebbed8ba02b582f558d6c4acd9be64601e0de8
realchayka <lonelyplay54@gmail.com>
2023-04-21 16:00:52 +0300

    Initial commit

---
+++ client/src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+import LongPulling from "./LongPulling.jsx";
+import EventSourcing from "./EventSourcing.jsx";
+import WebSock from "./WebSock.jsx";
+
+function App() {
+
+
+  return (
+   <div>
+       <WebSock />
+   </div>
+  )
+}
+
+export default App
